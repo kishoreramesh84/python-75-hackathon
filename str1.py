647e78a79425d244818978c9398c9e0e76861ece
kishoreramesh84 <kishoreramesh84@yahoo.com>
2018-12-14 21:18:42 +0530

    Length of a string using len function

---
+++ str1.py
@@ -0,0 +1,7 @@
+print("To calculate the length of a string")
+string1="Ramesh"
+string2="kishore"
+print("string1:",string1)
+print("string2:",string2)
+print("Length of string1:",len(string1))#In Python, it has default 'len' function to calculate length of a string
+print("Length of string2:",len(string2))
