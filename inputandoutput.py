04126bad70f379fb82fa8d00eb96aca11ef02725
kishoreramesh84 <kishoreramesh84@yahoo.com>
2018-12-14 11:32:28 +0530

    basic input and output statements with end and sep function

---
+++ inputandoutput.py
@@ -0,0 +1,14 @@
+print("Basics input and output statements")
+num1=10
+num2=20
+print("Printing the numbers without 'sep' and 'end' function")
+print(num1)
+print(num2)
+#In python it creates a default newline at end of each data in print statement
+print("Printing the numbers with 'end' function")
+print(num1,end=' ')
+print(num2,end=' ')
+#In python end is used to separate space,newline,..etc at end of print statement
+print("\nPrinting the numbers with 'sep' function")
+print(num1,num2,44,34,sep='!')
+#sep is used to separate between each data in print statement
